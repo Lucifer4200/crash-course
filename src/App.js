b7f3a0907cc86c53cf12f8412717085d5d5942a2
Md.Alamgir <alamgirwordpress1@gmail.com>
2021-02-24 17:55:45 +0600

    2nd update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import {useState} from 'react'
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+    const [tasks, setTask] = useState([
+      {
+          id : 1,
+          text : 'Doctors Appiontment',
+          day : 'Fed 5th at 2:30 pm',
+          reminder : true, 
+      },
+      {
+          id : 2,
+          text : 'Metting at school',
+          day : 'Fed 6th at 1:30 pm',
+          reminder : true, 
+      },
+      {
+          id : 3,
+          text : 'Food Shopping',
+          day : 'Fed 5th at 2:30 pm',
+          reminder : false, 
+      }
+  ])
+
+  // onDelete
+  const deleteTask = (id) => {
+    setTask(tasks.filter((task) => task.id !== id))
+  }
+
+  // toggle Riminder
+  const toggleReminder = (id) => {
+    console.log (id)
+  }
+
   return (
     <div className="container">
         <Header />
+        <Tasks 
+        tasks={tasks} 
+        onDelete={deleteTask}
+        onToggle={toggleReminder} />
     </div>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import Button from './Button'
 
 const Header = ({title}) => {
+    const  onClick = () => {
+        console.log('click')
+    }
     return (
         <header className='header'>
             <h2>{title}</h2>
-            <Button color='green' text='Hello' />
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
